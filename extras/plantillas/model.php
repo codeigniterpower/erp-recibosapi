9246a6cb4f47f367ae574263c2bf052aa22f5887
Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
2023-11-22 17:32:09 -0400

    Plantillas para la creacion de un nuevo modulo

---
+++ extras/plantillas/model.php
@@ -0,0 +1,28 @@
+<?php
+/*
+            ____                  _     _
+           / ___|_   _  __ _  ___| |__ (_)
+          | |  _| | | |/ _` |/ __| '_ \| |
+          | |_| | |_| | (_| | (__| | | | |
+           \____|\__,_|\__,_|\___|_| |_|_|
+Copyright (c) 2014  Díaz  Víctor  aka  (Máster Vitronic)
+Copyright (c) 2018  Díaz  Víctor  aka  (Máster Vitronic)
+<vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
+*/
+
+class XXX_model extends model {
+
+  private $sql;
+  private $err;
+
+  public function notFound() {
+    $this->borrow('notFound')->show();
+  }
+
+  private function save_ok() {}
+
+  public function save() {
+    print 'Hola Mundo!';
+  }
+
+}
